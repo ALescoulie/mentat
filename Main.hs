8f55affdf3e41d2462c658125f7e47d2068d5366
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-02-25 00:02:16 -0800

    add TokTree parser

--- Main.hs
+++ Main.hs
@@ -6,20 +6,20 @@ import Prelude hiding (lex)
 data Error
   = GotNegative
   | GotZero
+  | UnMatchedBracket
+  | UnfinishedTokenStream
 
 data Bracket = Curl | Sqr | Paren deriving (Show, Eq)
 
-data Op = Add | Sub | Mul | Div | Eql deriving (Show, Eq)
+data BinOp = Add | Sub | Mul | Div | Eql deriving (Show, Eq)
 
 bracket_chars = ['(', ')', '[', ']', '{', '}']
 
-op_chars = ['+', '-', '*', '/', '=']
+op_chars = ['+', '-', '*', '/', '^', '=']
 
 data Token
-  = TTrue
-  | TFalse
-  | TNumber Float
-  | TOp Op
+  = TNumber Float
+  | TOp BinOp
   | TOpen Bracket
   | TClose Bracket
   | TEq
@@ -81,6 +81,46 @@ matchSubStr [] x = (True, x)
 matchSubStr _ [] = (False, "")
 matchSubStr (x : xs) (y : ys) = if x == y then matchSubStr xs ys else (False, "")
 
+data TokTree = TLeaf Token | TNode Bracket [TokTree] deriving (Show, Eq)
+
+parseTokTree :: [Token] -> Either Error [TokTree]
+parseTokTree tokens = case parseInner tokens [] of
+  ([], Right t) -> Right t
+  (left, Right _) -> Left UnfinishedTokenStream
+  (_, Left err) -> Left err
+
+-- | Takes in a token stream, and a stack of brackets encountered so far.
+-- | Searches from start of token stream up until the next closing bracket.
+-- | Returns a tuple of the remaining tokens after the closing bracket, and the TokTree stream before.
+parseInner :: [Token] -> [Bracket] -> ([Token], Either Error [TokTree])
+parseInner [] [] = ([], Right [])
+parseInner [] y = ([], Left UnMatchedBracket)
+parseInner (TClose x : xs) (y : ys)
+  | x /= y = (TClose x : xs, Left UnMatchedBracket)
+  | otherwise = (xs, Right [])
+parseInner (TClose x : xs) [] = (xs, Left UnMatchedBracket)
+parseInner (TOpen x : xs) stack =
+  -- parse inner after open, parse rest, and stick the inner's result on before the rest
+  let (tokAfterInner, innerResult) = parseInner xs (x : stack)
+   in case innerResult of
+        Right inner ->
+          let innerElem = TNode x inner
+              (tokAfterRest, restResult) = parseInner tokAfterInner stack
+           in case restResult of
+                Right rest -> (tokAfterRest, Right (innerElem : rest))
+                Left err -> (tokAfterRest, Left err)
+        Left err -> (tokAfterInner, Left err)
+parseInner (x : xs) stack =
+  let (restTokens, restResult) = parseInner xs stack
+   in ( restTokens,
+        case restResult of
+          Right rest -> Right (TLeaf x : rest)
+          Left err -> Left err
+      )
+
+data Literal = BoolL Bool | RL Float
+
+data Expr = LitE Literal | VarE String | BinOpE BinOp [Expr]
 
 main = do
   print $ lex "3y = 12x + 5"
