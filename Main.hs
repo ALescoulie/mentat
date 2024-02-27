12c5876073b4b80e1a1fc503f426cd4eb3c9f74e
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-02-26 16:17:43 -0800

    add program parsing

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char (isAsciiLower, isAsciiUpper)
+import qualified Data.Map.Strict as HM
 import Prelude hiding (lex)
 
 data Error
@@ -11,10 +12,12 @@ data Error
   | UnfinishedTokenStream
   | BadToken [Token]
   | BadExpr [Expr]
+  | BadDecl [TokTree]
   | BadOp BinOp
   | EmptyExpr
   | LitBinOpError BinOp Literal Literal
-  
+  | ReasignError String
+
 instance Show Error where
   show GotNegative = "you dumbass you put in a negative"
   show GotZero = "you dumbass you put in a zero"
@@ -24,10 +27,12 @@ instance Show Error where
   show (BadExpr expr) = "You dumbass you put a bad expression.\nexpr" ++ show expr
   show (BadOp op) = "You dumbass you put a bad operator.\noperator: " ++ show op
   show (LitBinOpError op lExp rExp) = "You dumbass you mixed up reals and bools in: \nlExp " ++ show lExp ++ "\nop" ++ show op ++ " \nrExp" ++ show rExp
+  show (ReasignError var) = "You dumbass you reasigned a variable.\nvar" ++ show var
+  show (BadDecl stream) = "failed to parse decl " ++ show stream
 
 data Bracket = Curl | Sqr | Paren deriving (Show, Eq)
 
-data BinOp = Add | Sub | Mul | Div | Eql deriving (Show, Eq)
+data BinOp = Add | Sub | Mul | Div | Exp | Eql | GEq | LEq | NEq | L | G deriving (Show, Eq)
 
 bracket_chars = ['(', ')', '[', ']', '{', '}']
 
@@ -38,11 +43,11 @@ data Token
   | TOp BinOp
   | TOpen Bracket
   | TClose Bracket
-  | TEq
+  | TG
+  | TL
   | TId String
+  | TAsgn
   deriving (Show, Eq)
-
-
 
 safesqrt :: Float -> Either Error Float
 safesqrt x = if x >= 0 then Right (sqrt x) else Left GotZero
@@ -58,6 +63,7 @@ lex ('+' : cs) = TOp Add : lex cs
 lex ('-' : cs) = TOp Sub : lex cs
 lex ('*' : cs) = TOp Mul : lex cs
 lex ('/' : cs) = TOp Div : lex cs
+lex ('^' : cs) = TOp Exp : lex cs
 lex ('(' : cs) = TOpen Paren : lex cs
 lex (')' : cs) = TClose Paren : lex cs
 lex ('[' : cs) = TOpen Sqr : lex cs
@@ -65,6 +71,12 @@ lex (']' : cs) = TClose Sqr : lex cs
 lex ('{' : cs) = TOpen Curl : lex cs
 lex ('}' : cs) = TClose Curl : lex cs
 lex ('=' : cs) = TOp Eql : lex cs
+lex ('<' : '=' : cs) = TOp LEq : lex cs
+lex ('>' : '=' : cs) = TOp GEq : lex cs
+lex ('<' : cs) = TOp L : lex cs
+lex ('>' : cs) = TOp G : lex cs
+lex ('!' : '=' : cs) = TOp NEq : lex cs
+lex (':' : '=' : cs) = TAsgn : lex cs
 lex (c : cs)
   | isDigit c = let (n, rs) = lexNumber cs in TNumber (read (c : n)) : lex rs
   | otherwise = let (s, rs) = lexId cs in TId (c : s) : lex rs
@@ -127,28 +139,23 @@ parseInner (x : xs) stack = do
 
 data Literal = BoolL Bool | RL Float deriving (Show)
 
-binOpAdd :: Literal -> Literal -> Either Error Literal
-binOpAdd (RL x) (RL y) = Right (RL (x + y))
-binOpAdd x y = Left $ LitBinOpError Add x y
-
-binOpSub :: Literal -> Literal -> Either Error Literal
-binOpSub x (RL y) = binOpAdd x (RL (-y))
-binOpSub x y = Left $ LitBinOpError Sub x y
-
-binOpMul :: Literal -> Literal -> Either Error Literal
-binOpMul (RL x) (RL y) = Right (RL (x * y))
-binOpMul x y = Left $ LitBinOpError Mul x y
-
-binOpDiv :: Literal -> Literal -> Either Error Literal
-binOpDiv (RL x) (RL y) = case y == 0 of
+applyBinOp :: BinOp -> Literal -> Literal -> Either Error Literal
+applyBinOp Add (RL l) (RL r) = Right $ RL (l + r)
+applyBinOp Sub (RL l) (RL r) = Right $ RL (l - r)
+applyBinOp Mul (RL l) (RL r) = Right $ RL (l * r)
+applyBinOp Div (RL x) (RL y) = case y == 0 of
   True -> Left $ LitBinOpError Div (RL x) (RL y)
   False -> Right (RL (x / y))
-binOpDiv x y = Left $ LitBinOpError Div x y
-
-binOpEql :: Literal -> Literal -> Either Error Literal
-binOpEql (RL x) (RL y) = Right (BoolL (x == y))
-binOpEql (BoolL x) (BoolL y) = Right (BoolL (x == y))
-binOpEql x y = Left $ LitBinOpError Eql x y
+applyBinOp Exp (RL x) (RL y) = Right $ RL (x ** y)
+applyBinOp Eql (RL x) (RL y) = Right (BoolL (x == y))
+applyBinOp Eql (BoolL x) (BoolL y) = Right (BoolL (x == y))
+applyBinOp NEq (RL x) (RL y) = Right (BoolL (x /= y))
+applyBinOp NEq (BoolL x) (BoolL y) = Right (BoolL (x /= y))
+applyBinOp GEq (RL x) (RL y) = Right (BoolL (x >= y))
+applyBinOp G (RL x) (RL y) = Right (BoolL (x > y))
+applyBinOp LEq (RL x) (RL y) = Right (BoolL (x <= y))
+applyBinOp L (RL x) (RL y) = Right (BoolL (x < y))
+applyBinOp op x y = Left $ LitBinOpError op x y
 
 data Expr = LitE Literal | VarE String | BinOpE BinOp Expr Expr deriving (Show)
 
@@ -160,11 +167,12 @@ opPresidence Add = 1
 opPresidence Sub = 1
 opPresidence Mul = 2
 opPresidence Div = 2
-opPresidence Eql = 0
+opPresidence Exp = 3
+opPresidence op = 0
 
 opLeftAssoc :: BinOp -> Bool
 opLeftAssoc op
-  | op == Eql = False
+  | op == Exp = False
   | otherwise = True
 
 parseExpr :: [TokTree] -> Either Error Expr
@@ -188,6 +196,9 @@ shuntingYard (toT : restT) exprs ops = case toT of
     shuntingYard restT (innerExpr : exprs) ops
   _ -> Left EmptyExpr
 
+buildOpExpr :: BinOp -> Expr -> Expr -> Expr
+buildOpExpr op e1 e2 = if opLeftAssoc op then BinOpE op e1 e2 else BinOpE op e2 e1
+
 -- | Takes in an expresssion stack operator stack and a maybe BinOp of the last operator hit.
 -- | Returns the new Expression Stack and the new Operator Stack.
 combineExprs :: [Expr] -> [BinOp] -> Maybe BinOp -> Either Error ([Expr], [BinOp])
@@ -200,9 +211,9 @@ combineExprs exprs [] op = case op of -- If there are exprs left and no ops on t
   Just op -> Right (exprs, [op]) -- if op add to stack and return
 combineExprs (exprL : exprR : rest) (op2 : rops) maybeOp = case maybeOp of -- if there are enought expression on the stack
   Just op1 -> case popOp op1 op2 of
-    True -> combineExprs ((BinOpE (op2) exprL exprR) : rest) (rops) $ Just op1 -- If op1 has presidence over op2, combine the two expressions and put the new expression on the stack
+    True -> combineExprs (buildOpExpr op2 exprL exprR : rest) (rops) $ Just op1 -- If op1 has presidence over op2 combine the two expressions and put the new expression on the stack
     False -> Right ((exprL : exprR : rest), (op1 : op2 : rops))
-  Nothing -> do combineExprs ((BinOpE (op2) exprL exprR) : rest) rops Nothing
+  Nothing -> do combineExprs (buildOpExpr op2 exprL exprR : rest) rops Nothing
 combineExprs _ _ _ = Left EmptyExpr
 
 -- | Takes an operator op1 and the operator from the top of the stack op2
@@ -215,30 +226,15 @@ popOp op1 op2
     op1p = opPresidence op1
     op2p = opPresidence op2
 
-
 evalExpr :: Expr -> Either Error Literal
 evalExpr (LitE (RL n)) = Right (RL n)
 evalExpr (LitE (BoolL b)) = Right (BoolL b)
-evalExpr (VarE i) = Right (RL 0)  -- replace with var value later
+evalExpr (VarE i) = Right (RL 0) -- replace with var value later
 evalExpr (BinOpE op e1 e2) = do
   l1 <- evalExpr e1
   l2 <- evalExpr e2
-  case op of
-    Add -> do
-      result <- binOpAdd l1 l2
-      Right result
-    Sub -> do
-      result <- binOpSub l1 l2
-      Right result
-    Mul -> do
-      result <- binOpMul l1 l2
-      Right result
-    Div -> do
-      result <- binOpDiv l1 l2
-      Right result
-    Eql -> do
-      result <- binOpEql l1 l2
-      Right result
+  result <- applyBinOp op l1 l2
+  Right result
 
 buildExpr :: String -> Either Error Expr
 buildExpr [] = Left EmptyExpr
@@ -248,3 +244,99 @@ buildExpr str = do
   expr <- parseExpr tokTree
   Right expr
 
+newtype StrMap = StrMap {strMap :: HM.Map String Literal}
+
+parseDecl :: [TokTree] -> Either Error Statment
+parseDecl [] = Left EmptyExpr
+parseDecl (TLeaf (TId i) : TLeaf TAsgn : rest) = do
+  expr <- parseExpr rest
+  Right $ Declaration i expr
+parseDecl other = Left $ BadDecl other
+
+parseAsgn _ = Left EmptyExpr
+
+data Program = Program [Statment] deriving (Show)
+
+data Statment = Declaration String Expr | Constraint Expr deriving (Show)
+
+getProgramStatments :: Program -> [Statment]
+getProgramStatments (Program s) = s
+
+parseProgram :: [String] -> Either Error Program
+parseProgram [] = Right (Program [])
+parseProgram (s : ss) = do
+  let tokens = lex s
+  tokTree <- parseTokTree tokens
+  let decl = parseDecl tokTree
+  rest <- parseProgram ss
+  let restStatments = getProgramStatments rest
+  case decl of
+    Left _ -> do
+      expr <- parseExpr tokTree
+      Right $ Program (Constraint expr : restStatments)
+    Right decl -> do
+      rest <- parseProgram ss
+      Right $ Program (decl : restStatments)
+
+main = do
+  print "Parse into tokens"
+  print $ lex "3y = 12x + 5"
+  print $ lex "3y =(12x + 5"
+  print $ lex "3y = a(12x + 5)"
+  print $ lex "3y = a(b{c[12x + 5]})"
+  print $ lex "false 3y = true 12x + 5"
+  print $ lex "3y = 12x + 5true"
+
+  print "TokTree"
+  print $ parseTokTree $ lex "(x)"
+  print $ parseTokTree $ lex "(x + [x + {x + 1}])"
+  print $ parseTokTree $ lex "("
+  print $ parseTokTree $ lex ")"
+  print $ parseTokTree $ lex "([{}])"
+
+  print "Combine"
+  let ct1 = combineExprs [LitE (RL 1), LitE (RL 1)] [Add] $ Just Mul
+  print ct1
+
+  print "Shunting Yard"
+  let pt1 = parseTokTree $ lex "3 + 2"
+  print pt1
+  case pt1 of
+    Right tokTrees -> print $ parseExpr tokTrees
+    Left err -> print err
+
+  let pt2 = parseTokTree $ lex "3 + 2 * 4"
+  print pt2
+  case pt2 of
+    Right tokTrees -> print $ parseExpr tokTrees
+    Left err -> print err
+
+  let pt3 = parseTokTree $ lex "3 + 2 * (4 + 5)"
+  print pt3
+  case pt3 of
+    Right tokTrees -> print $ parseExpr tokTrees
+    Left err -> print err
+
+  let pt4 = parseTokTree $ lex "3 + 2^3 * 5"
+  case pt4 of
+    Right expr -> print $ parseExpr expr
+    Left err -> print err
+
+  print "Eval Expr"
+
+  let e1 = buildExpr "3 + 2"
+  case e1 of
+    Right expr -> print $ evalExpr expr
+    Left err -> print err
+
+  let e2 = buildExpr "3 + 2 * 4"
+  case e2 of
+    Right expr -> print $ evalExpr expr
+    Left err -> print err
+
+  let e3 = buildExpr "3 + 2^3 * 5^2"
+  case e3 of
+    Right expr -> print $ evalExpr expr
+    Left err -> print err
+
+  print $ parseProgram ["3 * y = 12 * x + 5", "y := 5", "x := 3"]
