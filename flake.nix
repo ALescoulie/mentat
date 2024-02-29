3281a8274b281b4eb71644a1fe06e58971928215
Alia Lescoulie <alia.lescoulie@gmail.com>
2024-02-29 15:51:20 -0800

    add hindent to flake

--- flake.nix
+++ flake.nix
@@ -18,19 +18,11 @@
     devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         buildInputs = [
-          pkgs.nodejs
-          # You can set the major version of Node.js to a specific one instead
-          # of the default version
-          # pkgs.nodejs-19_x
-
-          # You can choose pnpm, yarn, or none (npm).
-          pkgs.nodePackages.pnpm
-          # pkgs.yarn
-
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-          pkgs.python311
           pkgs.haskell.compiler.ghcjs
+          pkgs.haskellPackages.haskell-language-server
+          pkgs.haskellPackages.hindent
+          pkgs.ghc
+          pkgs.cabal-install
         ];
       };
     });
